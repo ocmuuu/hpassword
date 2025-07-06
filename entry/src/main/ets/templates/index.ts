8485356f960fff50faec2efb4d3a515bae7c8ce9
Mznm <service@mznm.com>
2025-07-06 14:35:41 +0800

    update database init and group display

--- entry/src/main/ets/templates/index.ts
+++ entry/src/main/ets/templates/index.ts
@@ -1,3 +1,59 @@
+import { LoginTemplate } from './Login';
+import { SecureNoteTemplate } from './SecureNote';
+import { BankAccountTemplate } from './BankAccount';
+import { PaymentAccountTemplate } from './PaymentAccount';
+import { SecuritiesFundTemplate } from './SecuritiesFund';
+import { EmailAccountTemplate } from './EmailAccount';
+import { MembershipCardTemplate } from './MembershipCard';
+import { SubscriptionTemplate } from './Subscription';
+import { AddressBookTemplate } from './AddressBook';
+import { FamilyTemplate } from './Family';
+import { CertificateTemplate } from './Certificate';
+import { InsurancePolicyTemplate } from './InsurancePolicy';
+import { UtilityAccountTemplate } from './UtilityAccount';
+import { MedicalRecordTemplate } from './MedicalRecord';
+import { LoyaltyProgramTemplate } from './LoyaltyProgram';
+import { KeyCertificateTemplate } from './KeyCertificate';
+import { ServerTemplate } from './Server';
+import { DatabaseTemplate } from './Database';
+import { IoTDeviceTemplate } from './IoTDevice';
+import { VehicleInfoTemplate } from './VehicleInfo';
+import { RouterTemplate } from './Router';
+import { ApiCredentialTemplate } from './ApiCredential';
+import { SoftwareLicenseTemplate } from './SoftwareLicense';
+import { CryptoWalletTemplate } from './CryptoWallet';
+import { OtherTemplate } from './Other';
+import { CategoryTemplate } from './TemplateTypes';
+
+/** 所有分类模板集合，便于批量初始化测试数据 */
+export const ALL_TEMPLATES: CategoryTemplate[] = [
+  LoginTemplate,
+  SecureNoteTemplate,
+  BankAccountTemplate,
+  PaymentAccountTemplate,
+  SecuritiesFundTemplate,
+  EmailAccountTemplate,
+  MembershipCardTemplate,
+  SubscriptionTemplate,
+  AddressBookTemplate,
+  FamilyTemplate,
+  CertificateTemplate,
+  InsurancePolicyTemplate,
+  UtilityAccountTemplate,
+  MedicalRecordTemplate,
+  LoyaltyProgramTemplate,
+  KeyCertificateTemplate,
+  ServerTemplate,
+  DatabaseTemplate,
+  IoTDeviceTemplate,
+  VehicleInfoTemplate,
+  RouterTemplate,
+  ApiCredentialTemplate,
+  SoftwareLicenseTemplate,
+  CryptoWalletTemplate,
+  OtherTemplate
+];
+
 export * from './TemplateTypes';
 export * from './Login';
 export * from './SecureNote';
